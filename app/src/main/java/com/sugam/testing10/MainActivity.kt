5f959d224873535e59285693c4eb89b80d088b3c
SUGAM <sugamshaw2002@gmail.com>
2023-09-09 20:32:15 +0530

    i m in main branch

--- app/src/main/java/com/sugam/testing10/MainActivity.kt
+++ app/src/main/java/com/sugam/testing10/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        println("i m in master branch")
         println("i am in yt branch")
     }
 }
